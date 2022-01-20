1d1df1a9e6e1551c9ea7f99a5ba87617243ced60
yi113 <yiyang2222@gmail.com>
2022-01-20 11:23:58 -0800

    improved version

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,7 +15,8 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(openParen - nextCloseBracket == 1)
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
         return toReturn;
