f745761c5c497cce0ad484fea647ff8438ddfa29
Andrew <Cubified@users.noreply.github.com>
2022-01-26 14:49:06 -0800

    Exception in thread "main" java.lang.StringIndexOutOfBoundsException: begin 4, end -1, length 6 -> Fix out of bounds exception on missing closing parenthesis

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,11 +14,12 @@ public class MarkdownParse {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
+            int closeParen = markdown.indexOf(")", openParen);
 
             if(nextOpenBracket >= 0 &&
                nextCloseBracket >= 0 &&
-               openParen == nextCloseBracket+1){
-                int closeParen = markdown.indexOf(")", openParen);
+               openParen == nextCloseBracket+1 &&
+               closeParen >= 0){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
             } else {
