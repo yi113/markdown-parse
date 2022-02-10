5786768cc77996c2b8e72297aa4a93caa19f6e00
yi113 <yiyang2222@gmail.com>
2022-02-10 15:47:41 -0800

    adding a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,7 +8,7 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
-        // the next )
+        // the next
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
